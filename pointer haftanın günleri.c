4c90859e8ff67505a016c2543ee9cd96dc00b597
Muzaffer Deliali <46688496+MuzaffeerD@users.noreply.github.com>
2021-04-13 17:18:39 +0300

    Add files via upload

---
+++ pointer haftanın günleri.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+char *gunismi(char *gundizisi[],int uzunluk,int hangigun){
+	
+	if (hangigun >1 && 	hangigun <=7) {
+		
+		return gundizisi[hangigun-1];
+	}
+	else {
+		return NULL;
+	}
+}
+
+int main(){
+	
+	char *gunler[7] = {"Pazartesi","Sali","Carsamba","Persembe","Cuma","Cumartesi","Pazar"};
+	
+	char *p = gunismi(gunler,7,5);
+	
+	if (p == NULL) {
+		printf ("NULL");
+	}
+	
+	else {
+		
+		printf ("%s",p);
+	}
+	
+	return 0;
+}
