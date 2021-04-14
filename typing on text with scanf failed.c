bb1825a30f7a2fdc5289d127d7b19349271b7d3a
Muzaffer Deliali <46688496+MuzaffeerD@users.noreply.github.com>
2021-04-14 17:51:30 +0300

    Add files via upload

---
+++ typing on text with scanf failed.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char data[25];
+	int length = strlen(data);
+	int i;	
+	
+	printf("What do you want to write?");
+	scanf("%s",data);
+
+	
+	FILE *filep = fopen("My_Name.txt","w");
+	
+	if (filep == NULL) {
+		printf("I am unable to create txt file");
+	}
+	else {
+		for(i=0; i < length;i++){
+			
+			fputc(data[i],filep);
+			printf("Typed character %c\n",data[i]);
+		};
+		printf("I successfully type that string");
+		fclose(filep);
+	}
+}
